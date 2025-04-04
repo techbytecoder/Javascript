506ea17fc319c6e82a3c40e8717e532ff9906f66
techbytecoder <techbytecoder@outlook.com>
2025-04-04 03:04:25 -0700

    Add basic JavaScript dialog box examples
    - Includes alert(), confirm(), and prompt() demos
    - HTML and JS files structured for clarity

---
+++ DialogBoxes/JS1.js
@@ -0,0 +1,7 @@
+alert('Hello welcome to the world of javascript!')
+
+let userclick = confirm('OK or CANCEL');
+alert('User clicked:'+userclick);
+
+let username = prompt();
+document.write(username)
