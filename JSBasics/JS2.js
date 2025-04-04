9e13c1549c0c89538109b7c2d1c7a05d284aab90
techbytecoder <techbytecoder@outlook.com>
2025-04-04 03:21:38 -0700

    JavaScript basics examples
     - document.write() and document.writeln() demos  
    - Escape sequences (\n, \t, \', \") usage  
    - Simple HTML structure for testing

---
+++ JSBasics/JS2.js
@@ -0,0 +1,7 @@
+document.write('Good Afternoon!<br/>');
+document.write('Javascript is considered <br/>&nbsp;&nbsp;&nbsp;&nbsp; as client side scripting language!<br/>');
+document.write('Hello! \n welcome to javascript!<br/>');
+document.write("Path: C:\\Windows\\System32");
+
+
+// \n and \t does not work as expected so sometimes you have to use html tags!
---
+++ JSBasics/JS3.js
@@ -0,0 +1,12 @@
+let js = 'amazing';
+if (js === 'amazing')alert('Javascript is fun to learn!')
+
+console.log(40+20+3);
+console.log('Jonas');
+
+let fname = 'Harry';
+document.write(fname);
+let lname = prompt('Enter your last name');
+document.write(' '+lname);
+
+//name conventions in javascript variable names are declared with camel case. eg: firstName or lastName
